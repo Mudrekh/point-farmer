5b53b506e373c684a40eed428e5198b2cf852f15
Mudrekh Goderya <mudrekh@gmail.com>
2022-04-14 22:53:06 -0500

    Add bot oauth validation

--- farmer.js
+++ farmer.js
@@ -1,7 +1,10 @@
-const config = require('config');
-const { client: Client } = require('tmi.js');
-const debug = require('debug');
 
+import config from 'config';
+import { client as Client } from 'tmi.js';
+import _debug from 'debug';
+import got from 'got';
+
+const debug = _debug('farmer');
 // Define configuration options
 const opts = {
   identity: {
@@ -11,6 +14,15 @@ const opts = {
   channels: config.get('channels')
 };
 
+function log(type, ...args) {
+  if (!console[type]) {
+    args.unshift(type);
+    type = 'log';
+  }
+  console[type](new Date().toISOString(), ' - ', ...args);
+}
+
+
 // Create a client with our options
 const client = new Client(opts);
 
@@ -19,8 +31,13 @@ client.on('message', onMessageHandler);
 client.on('connected', onConnectedHandler);
 
 // Connect to Twitch:
-console.log(`${new Date().toISOString()} - Start`);
+log('Start');
 client.connect();
+
+// https://dev.twitch.tv/docs/authentication/validate-tokens
+validate(config.get('password'));
+setInterval(() => validate(config.get('password')), 1000 * 60 * 60);
+
 
 let farmed = 0;
 // Called every time a message comes in
@@ -34,13 +51,27 @@ function onMessageHandler(target, context, msg, isBot) {
   const [cmd, , points] = commandName.split(' ');
   debug(target, context, msg, isBot);
   if (cmd === '!addpoints') {
-    console.log(`${new Date().toISOString()} - Channel: ${target}, Points: ${points}`);
-    farmed += Number(points);
-    console.log(`${new Date().toISOString()} - Total Farmed: ${farmed}`);
+    log(`${new Date().toISOString()} - Channel: ${target}, Points: ${points}`);
+    if (Number(points)) farmed += Number(points);
+    log(`${new Date().toISOString()} - Total Farmed: ${farmed}`);
   }
 }
 
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler(addr, port) {
-  console.log(`${new Date().toISOString()} - * Connected to ${addr}:${port}`);
+  log(`${new Date().toISOString()} - * Connected to ${addr}:${port}`);
+}
+
+async function validate(token) {
+  try {
+    const [, str] = token.split(':');
+    const resp = await got('https://id.twitch.tv/oauth2/validate', {
+      headers: {
+        authorization: `OAuth ${str}`
+      }
+    });
+    debug(resp.body.trim());
+  } catch (err) {
+    log('error', `Error validating token: ${err}`);
+  }
 }
